9780707371ba48050d767ebb7f5002487947f0e1
Freyj <aerandil@gmail.com>
2017-12-22 20:58:32 +0100

    commit wip
    j'aimeposwing

--- src/client/ParamsConfig.java
+++ src/client/ParamsConfig.java
@@ -10,17 +10,25 @@ public class ParamsConfig {
 	
 	
 	//textes pour les labels 
-	static String CHRONO = "Chronomètre"; 
-	static String CATALOGUE = "Catalogue";
 	static String CHOIX_PSEUDO = "Choisissez votre pseudo";
 	static String SERVER = "Connexion au serveur : localhost:8090/enchere";
+	
+	static String LABEL_NOM = "Nom";
+	static String LABEL_OBJET_COURANT = "Objet Courant";
+	static String LABEL_DESCRIPTION = "Description";
+	static String LABEL_PRIX_ACTUEL = "Prix Actuel";
+	static String LABEL_GAGNANT = "Gagnant";
+
+	static String CHRONO = "Chronomètre"; 
+	static String CATALOGUE = "Catalogue";
+	
 	
 	//textes pour les boutons
 	static String BUTTON_INSCRIPTION = "Inscription";
 	
 	static String BUTTON_ENCHERIR = "Encherir";
 	static String BUTTON_SOUMETTRE_ENCHERE = "Soumettre une enchère";
-	static String BUTTON_SOUMETTRE_OBJET = "Soumettre"; //"un objet"
+	static String BUTTON_SOUMETTRE_OBJET = "Soumettre un objet"; //"un objet"
 	static String BUTTON_PASSER = "Passer";
 	
 	static String ERROR_TITLE = "Problème";
