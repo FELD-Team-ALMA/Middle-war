8bc31a9b714cb1c5fdc8e2ed84fecd03b41bfa7a
Araya Montalvo <montalvo@orange.fr>
2017-12-23 16:50:03 +0100

    Javadoc Objet

--- src/serveur/Objet.java
+++ src/serveur/Objet.java
@@ -1,7 +1,12 @@
 package serveur;
 
 import java.io.Serializable;
-
+/**
+ * Classe représentant les objets mis aux enchères. 
+ * Implémente Serializable
+ * @author Léo CASSIAU, Geoffrey DESBROSSES, Jean-Christophe GUERIN, Ugo MAHEY
+ *
+ */
 public class Objet implements Serializable{
 
 	private static final long serialVersionUID = 1L;
@@ -13,7 +18,14 @@ public class Objet implements Serializable{
 	private String gagnant;
 	
 	
-	
+	/**
+	 * Constructeur d'un objet. 
+	 * disponible initialisé à true.
+	 * gagnant initialisé à "" 
+	 * @param nom : nom de l'objet
+	 * @param description : description  de l'objet
+	 * @param prixBase : prix de départ de l'objet
+	 */
 	public Objet(String nom, String description, int prixBase) {
 		super();
 		this.nom = nom;
@@ -23,42 +35,90 @@ public class Objet implements Serializable{
 		this.disponible = true;
 		this.gagnant = "";
 	}
-	
+	/**
+	 * Get Nom de l'objet
+	 * @return String : le nom
+	 */
 	public String getNom() {
 		return nom;
 	}
+	/**
+	 * Set nom de l'objet
+	 * @param nom : le nouveau nom.
+	 */
 	public void setNom(String nom) {
 		this.nom = nom;
 	}
+	/**
+	 * Get description de l'objet
+	 * @return String : la déscription
+	 */
 	public String getDescription() {
 		return description;
 	}
+	/**
+	 * Set description de l'objet.
+	 * @param description : la nouvelle déscription.
+	 */
 	public void setDescription(String description) {
 		this.description = description;
 	}
+	/**
+	 * Get le prix de base de l'objet
+	 * @return int : le prix de base
+	 */
 	public int getPrixBase() {
 		return prixBase;
 	}
+	/**
+	 * Set le prix de base de l'objet
+	 * @param prixBase : le nouveau prix de base
+	 */
 	public void setPrixBase(int prixBase) {
 		this.prixBase = prixBase;
 	}
+	/**
+	 * Dit si l'objet est disponible
+	 * @return boolean : return true si l'objet est dispo. False sinon.
+	 */
 	public boolean isDisponible() {
 		return disponible;
 	}
+	/**
+	 * Set la disponibilité de l'objet
+	 * @param disponible : true si on veut l'objet dispo. False sinon.
+	 */
 	public void setDisponible(boolean disponible) {
 		this.disponible = disponible;
 	}
+	/**
+	 * Get le prix courant de l'objet
+	 * @return int : le prix courant
+	 */
 	public int getPrixCourant() {
 		return prixCourant;
 	}
+	/**
+	 * Set le prix courant de l'objet
+	 * @param prixCourant : le nouveau prix courant
+	 */
 	public void setPrixCourant(int prixCourant) {
 		this.prixCourant = prixCourant;
 	}
 
+	/**
+	 * Get le gagnant actuelle de l'objet. 
+	 * C'est à dire celui qui va avoir l'objet si personne enchérie.
+	 * @return String : le pseudo du gagnant
+	 */
 	public String getGagnant() {
 		return gagnant;
 	}
 
+	/**
+	 * Set le gagnant de l'objet
+	 * @param gagnant : le pseudo du nouveau gagnant.
+	 */
 	public void setGagnant(String gagnant) {
 		this.gagnant = gagnant;
 	}
