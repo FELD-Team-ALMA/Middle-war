9c9dc308abd5546c441023dee96bab00a5dd8024
Araya Montalvo <montalvo@orange.fr>
2017-12-23 15:04:24 +0100

    Javadoc EtatClient.

--- src/client/EtatClient.java
+++ src/client/EtatClient.java
@@ -1,5 +1,10 @@
 package client;
-
+/**
+ * Énumération des états possibles des clients.
+ * État possibles : ATTENTE,RENCHERI,TERMINE
+ * @author Léo CASSIAU, Geoffrey DESBROSSES, Jean-Christophe GUERIN, Ugo MAHEY
+ *
+ */
 public enum EtatClient {
 	ATTENTE,
 	RENCHERI,
