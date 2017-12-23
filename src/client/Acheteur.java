08c062e082b996d083e4d11c9b5ba013d2d5ff78
Araya Montalvo <montalvo@orange.fr>
2017-12-23 13:42:36 +0100

    Javadoc Acheteur

--- src/client/Acheteur.java
+++ src/client/Acheteur.java
@@ -3,9 +3,15 @@ package client;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 
+/**
+ * Interface décrivant le comportement d'un acheteur.
+ * @author Léo CASSIAU, Geoffrey DESBROSSES, Jean-Christophe GUERIN, Ugo MAHEY
+ * 
+ */
 public interface Acheteur extends Remote {
 
 	/**
+	 * Vend l'objet au gagnant de l'enchère.
 	 * 
 	 * @param gagnant
 	 *            Client qui a gagne l'enchere
@@ -13,12 +19,35 @@ public interface Acheteur extends Remote {
 	 */
 	public void objetVendu(String gagnant) throws RemoteException;
 
+	/**
+	 * Attribut un nouveau prix à un objet pendant une enchère. Met aussi à jours la personne ayant 
+	 * la plus grosse enchère sur l'objet.
+	 * 
+	 * @param prix = Nouveau prix de l'objet
+	 * @param gagnant = Acheteur ayant mis le nouveau prix. Donc le gagnant si personne renchérit.
+	 * @throws RemoteException
+	 */
 	public void nouveauPrix(int prix, Acheteur gagnant) throws RemoteException;
 	
+	/**
+	 * Met fin à une enchère
+	 * @throws RemoteException
+	 */
 	public void finEnchere() throws RemoteException;
-
+	
+	/**
+	 * Récupère le pseudo de l'acheteur.
+	 * 
+	 * @return String : le pseudo de l'acheteur
+	 * @throws RemoteException
+	 */
 	public String getPseudo() throws RemoteException;
 
+	/**
+	 * Récupère le chronomètre de l'enchère à la quelle participe l'acheteur
+	 * @return long : la durée du chrono
+	 * @throws RemoteException
+	 */
 	public long getChrono() throws RemoteException;
 
 
